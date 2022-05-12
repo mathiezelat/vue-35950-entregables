28a91a325b13be8057ba8a7f51cf150b9abc00ad
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2022-05-12 20:59:30 -0300

    entregable - clase 6

---
+++ entregable-clase-06/src/App.vue
@@ -0,0 +1,147 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Clase 06 - Entregable" />
+    <div class="container">  
+      <TableComponent 
+        :titles="italian.titles"
+        :products="italian.products"
+        :cssClasses="italian.cssClasses"
+      />
+      <TableComponent 
+        :titles="drinks.titles"
+        :products="drinks.products"
+        :cssClasses="drinks.cssClasses"
+      />
+      <TableComponent 
+        :titles="desserts.titles"
+        :products="desserts.products"
+        :cssClasses="desserts.cssClasses"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import TableComponent from "./components/TableComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    TableComponent,
+  },
+  data() {
+    return {
+      italian: {
+        titles: ["Id", "Name", "Description", "Price"],
+        products: [
+          {
+            id: 1,
+            name: "Spaghetti alla puttanesca",
+            description:
+              "Es un plato elaborado con pasta espagueti y con una salsa denominada sugo alla puttanesca",
+            price: 575.0,
+          },
+          {
+            id: 2,
+            name: "Pizza Napoletana ai carciofi",
+            description:
+              "É una variante deliziosa della classica pizza, una ricetta dal sapore delicato arricchita con filante mozzarella di bufala",
+            price: 675.0,
+          },
+          {
+            id: 3,
+            name: "Porchetta umbra a cottura lunga",
+            description:
+              "É un piatto tipico dell'Italia centrale e di poche regioni settentrionali",
+            price: 845.0,
+          },
+          {
+            id: 4,
+            name: "Orecchiette alle cime di rapa",
+            description:
+              "Un plato típico de la cocina de Puglia , elaborado con ingredientes naturales",
+            price: 845.0,
+          },
+        ],
+        cssClasses: {
+          "table-dark": true,
+          "text-warning": true,
+          "table-striped": true,
+        },
+      },
+      drinks: {
+        titles: ["Id", "Name", "Price"],
+        products: [
+          {
+            id: 1,
+            name: "Pepsi 1L",
+            price: 100,
+          },
+          {
+            id: 2,
+            name: "Coca-Cola 1.25L",
+            price: 115,
+          },
+          {
+            id: 3,
+            name: "Fanta 600ML",
+            price: 65,
+          },
+          {
+            id: 4,
+            name: "Sprite 2L",
+            price: 200,
+          },
+        ],
+        cssClasses: {
+          "table-warning": true,
+          "text-danger": true,
+        },
+      },
+      desserts: {
+        titles: ["Id", "Name", "Price"],
+        products: [
+          {
+            id: 1,
+            name: "Kunāfah",
+            price: 140,
+          },
+          {
+            id: 2,
+            name: "Torrone",
+            price: 50,
+          },
+          {
+            id: 3,
+            name: "Île flottante",
+            price: 110,
+          },
+          {
+            id: 4,
+            name: "Appeltaart",
+            price: 200,
+          },
+        ],
+        cssClasses: {
+          "table-danger": true,
+          "text-info": true,
+          "table-hover": true,
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ entregable-clase-06/src/components/TableComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div align="col">
+    <table class="table" :class="cssClasses">
+      <thead>
+        <tr>
+          <th scope="col" v-for="(title, index) in titles" :key="index">
+            {{ title }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <td v-for="(characteristic, idx) in product" :key="idx">
+            {{ characteristic }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TableComponent",
+  props: {
+    titles: {
+      type: Array,
+      required: true,
+    },
+    products: {
+      type: Array,
+      required: true,
+    },
+    cssClasses: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
